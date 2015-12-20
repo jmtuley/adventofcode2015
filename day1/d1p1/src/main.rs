ddaa4fddab1ebdff39ea093793dbd649feb051d6
John Tuley <john@tuley.org>
2015-12-20 16:07:40 -0700

    Solve day 1

--- day1/d1p1/src/main.rs
+++ day1/d1p1/src/main.rs
@@ -1,3 +1,33 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut input = String::new();
+    match io::stdin().read_line(&mut input) {
+        Err(error) => { panic!(error) }
+        _ => {}
+    };
+
+    let mut position = 0;
+    for c in input.chars() {
+        match c {
+            '(' => { position += 1; }
+            ')' => { position -= 1; }
+            _ => {}
+        }
+    }
+
+    println!("Final position is {}.", position);
+
+    position = 0;
+    for (i,c) in input.chars().enumerate() {
+        match c {
+            '(' => { position += 1; }
+            ')' => { position -= 1; }
+            _ => {}
+        }
+        if position == -1 {
+            println!("Entered basement at {}.", i + 1);
+            break;
+        }
+    }
 }
